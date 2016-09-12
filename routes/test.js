23e14af88f0ac229e375d5d6a064ec6135c9c1ec
JonOsment <k08jo01@gmail.com>
2016-09-12 18:48:37 -0400

    Changes to add a test file

--- routes/test.js
+++ routes/test.js
@@ -3,8 +3,9 @@ var users = require('../Controllers/UserController.js');
 var router = express.Router();
 var mongoose = require('mongoose');
 var TeamMember = mongoose.model('TeamMembers');
+var util = require('util')
 
 /* GET users listing. */
-router.post('/', function(req, res) { console.log(req.body); });
+router.post('/', function(req, res) { console.log(util.inspect(req.body, false, null)) });
 
 module.exports = router;
