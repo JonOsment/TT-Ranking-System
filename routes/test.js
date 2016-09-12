2b73ddb694b91230e963d862841a65fff2223745
JonOsment <k08jo01@gmail.com>
2016-09-12 18:37:15 -0400

    Changes to add a test file

--- routes/test.js
+++ routes/test.js
@@ -5,6 +5,6 @@ var mongoose = require('mongoose');
 var TeamMember = mongoose.model('TeamMembers');
 
 /* GET users listing. */
-router.get('/', function(req, res) { console.log(req); });
+router.get('/', function(req, res) { console.log(req.body); });
 
 module.exports = router;
